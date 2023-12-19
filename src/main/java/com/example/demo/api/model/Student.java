8eb0643c6a7020bb29c1b41b7deaaff0fece0080
Minh Phạm Quang <82825240+Minhqp1512@users.noreply.github.com>
2023-12-19 13:58:43 +0700

    Update Student.java

--- src/main/java/com/example/demo/api/model/Student.java
+++ src/main/java/com/example/demo/api/model/Student.java
@@ -1,2 +1,86 @@
-package com.example.demo.api.model;public class Student {
+package com.example.demo.api.model;
+public class Student {
+    private int id;
+    private String name;
+    private String birthday;
+    private String sex;
+    private String address;
+    private double math;
+    private double english;
+    private double literature;
+
+    public Student(int id, String name, String birthday, String sex, String address, double math, double english, double literature) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.sex = sex;
+        this.address = address;
+        this.math = math;
+        this.english = english;
+        this.literature = literature;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getMath() {
+        return math;
+    }
+
+    public void setMath(double math) {
+        this.math = math;
+    }
+
+    public double getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(double english) {
+        this.english = english;
+    }
+
+    public double getLiterature() {
+        return literature;
+    }
+
+    public void setLiterature(double literature) {
+        this.literature = literature;
+    }
 }
