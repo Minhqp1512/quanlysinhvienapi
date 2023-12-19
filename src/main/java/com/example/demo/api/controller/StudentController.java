2c49f4269b9f09c2fe1f3a0febfe5983c241e0ce
Minh Phạm Quang <82825240+Minhqp1512@users.noreply.github.com>
2023-12-19 13:44:26 +0700

    Update StudentController.java

--- src/main/java/com/example/demo/api/controller/StudentController.java
+++ src/main/java/com/example/demo/api/controller/StudentController.java
@@ -1,2 +1,25 @@
-package com.example.demo.api.controller;public class StudentController {
+package com.example.demo.api.controller;
+import com.example.demo.api.model.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService=studentService;
+    }
+    @GetMapping("/student")
+    public Student getStudent(@RequestParam Integer id){
+        Optional<Student> student= studentService.getStudent(id);
+        return (Student) student.orElse(null);
+    }
+
 }
